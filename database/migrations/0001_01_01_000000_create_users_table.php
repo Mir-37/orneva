8dcc4ee7c1c93734e38c6e10a143b870f209ed2c
Mir-37 <mirmutasim37@gmail.com>
2025-08-22 16:27:58 +0600

    modify schema and add seeder

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('photo');
+            $table->string('photo')->nullable();
             $table->enum('role', ['admin', 'customer'])->default('customer');
             $table->string('phone');
             $table->string('email')->unique()->nullable();
